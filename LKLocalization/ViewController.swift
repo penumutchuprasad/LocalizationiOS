f3bf93c982ff4b711e82ba600c1ae0c00537e801
leelaprasad <leela@secninjaz.com>
2018-05-18 16:59:58 +0530

    Initial Commit

---
+++ LKLocalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LKLocalization
+//
+//  Created by Leela Prasad on 18/05/18.
+//  Copyright © 2018 Leela Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
